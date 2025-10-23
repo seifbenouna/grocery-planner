4c8fd0177992b42ddc754a08a6dc107c0005ab92
seifbenouna <seifdeveloper99@gmail.com>
2025-10-23 09:56:41 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/grocery-planner/" : "/",
-  // base: "/grocery-planner/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 }));
